126b8e931cb49d14bad9144a409c79aead1b2ccc
YukiTakazawa <yukilbx0612@gmail.com>
2019-11-23 01:36:17 +0900

    もっと簡単な方法でPlaceHolderを実装できた

---
+++ EXTextView2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EXTextView2
+//
+//  Created by kiyolab01 on 2019/11/23.
+//  Copyright © 2019 takazawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
